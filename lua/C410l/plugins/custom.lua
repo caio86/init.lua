779a9583265d0d856bff9fb3a45f224054dc2e6a
caio86 <caioluiz86@gmail.com>
2024-03-03 17:59:12 -0300

    add lsp-zero plugin

--- lua/C410l/plugins/custom.lua
+++ lua/C410l/plugins/custom.lua
@@ -1,28 +1,11 @@
 local plugins = {
   {
-    "theprimeagen/harpoon",
-    config = function()
-      local mark = require("harpoon.mark")
-      local ui = require("harpoon.ui")
-      local wk = require("which-key")
-
-      wk.register({
-        ["<leader>a"] = { mark.add_file, "Harpoon Add File"},
-        ["<C-e>"] = { ui.toggle_quick_menu, "Harpoon Toggle Quick Menu"},
-
-        ["<leader>1"] = { function()
-          ui.nav_file(1)
-        end, "Harpoon File 1"},
-
-        ["<leader>2"] = { function()
-          ui.nav_file(2)
-        end, "Harpoon File 2"},
-
-        ["<leader>3"] = { function()
-          ui.nav_file(3)
-        end, "Harpoon File 3"},
-      })
-    end
+    {
+      'echasnovski/mini.comment', version = '*',
+      config = function()
+        require("mini.comment").setup()
+      end
+    },
   },
   {
     "mbbill/undotree",
--- lua/C410l/plugins/editor.lua
+++ lua/C410l/plugins/editor.lua
@@ -27,4 +27,29 @@ return {
       }, { prefix = "<leader>" })
     end
   },
+  {
+    "theprimeagen/harpoon",
+    config = function()
+      local mark = require("harpoon.mark")
+      local ui = require("harpoon.ui")
+      local wk = require("which-key")
+
+      wk.register({
+        ["<leader>a"] = { mark.add_file, "Harpoon Add File"},
+        ["<C-e>"] = { ui.toggle_quick_menu, "Harpoon Toggle Quick Menu"},
+
+        ["<leader>1"] = { function()
+          ui.nav_file(1)
+        end, "Harpoon File 1"},
+
+        ["<leader>2"] = { function()
+          ui.nav_file(2)
+        end, "Harpoon File 2"},
+
+        ["<leader>3"] = { function()
+          ui.nav_file(3)
+        end, "Harpoon File 3"},
+      })
+    end
+  },
 }

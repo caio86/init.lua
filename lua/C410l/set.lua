b4c05ea5a1ec058657a3a91b273126ee4d8ebb98
caio86 <caioluiz86@gmail.com>
2024-03-03 11:28:12 -0300

    edit netrw

--- lua/C410l/set.lua
+++ lua/C410l/set.lua
@@ -2,6 +2,11 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+-- edit netrw browser
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+vim.g.netrw_winsize = 25
+
 local opt = vim.opt
 
 -- line numbers

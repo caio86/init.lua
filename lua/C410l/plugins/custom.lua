282750aa5fa3b51bff46c42e5ca3bf0bbdb2ae15
caio86 <caioluiz86@gmail.com>
2024-03-06 19:29:02 -0300

    add vim-tmux-navigator

--- lua/C410l/plugins/custom.lua
+++ lua/C410l/plugins/custom.lua
@@ -10,6 +10,15 @@ local plugins = {
       vim.g.startuptime_tries = 10
     end,
   },
+  {
+    "christoomey/vim-tmux-navigator",
+    keys = {
+      { "<C-h>", "<cmd>TmuxNavigateLeft<CR>", desc = { "Window left" } },
+      { "<C-l>", "<cmd>TmuxNavigateRight<CR>", desc = { "Window right" } },
+      { "<C-j>", "<cmd>TmuxNavigateDown<CR>", desc = { "Window down" } },
+      { "<C-k>", "<cmd>TmuxNavigateUp<CR>", desc = { "Window up" } },
+    },
+  },
 }
 
 return plugins

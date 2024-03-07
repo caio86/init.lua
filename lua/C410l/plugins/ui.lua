62c39b594da44493ecbc817c3db71bcd473db3d8
caio86 <caioluiz86@gmail.com>
2024-03-07 18:13:37 -0300

    add lualine plugin

--- lua/C410l/plugins/ui.lua
+++ lua/C410l/plugins/ui.lua
@@ -121,6 +121,29 @@ return {
 			})
 		end,
 	},
+  {
+    'nvim-lualine/lualine.nvim',
+    event = "VeryLazy",
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    init = function()
+      vim.g.lualine_laststatus = vim.o.laststatus
+      if vim.fn.argc(-1) > 0 then
+        -- set an empty statusline till lualine loads
+        vim.o.statusline = " "
+      else
+        -- hide the statusline on the starter page
+        vim.o.laststatus = 0
+      end
+    end,
+    opts = {
+      options = {
+        theme = "auto",
+        command_separators = "|",
+        globalstatus = true,
+        disabled_filetypes = { statusline = { "dashboard", "alpha", "starter" } },
+      }
+    },
+  },
 	{
 		"nvimdev/dashboard-nvim",
 		event = "VimEnter",

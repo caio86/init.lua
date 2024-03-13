e7dbb986c60b48350e724a157cce4089d1484167
caio86 <caioluiz86@gmail.com>
2024-03-13 16:58:39 -0300

    add luasnip configs on others.lua

--- lua/C410l/plugins/lsp.lua
+++ lua/C410l/plugins/lsp.lua
@@ -34,15 +34,14 @@ return {
   {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
-    dependencias = {
+    dependencies = {
       {
         -- snippet plugin
         "L3MON4D3/LuaSnip",
         dependencies = "rafamadriz/friendly-snippets",
         opts = { history = true, updateevents = "TextChanged, TextChangedI" },
         config = function(_, opts)
-          require("luasnip").config.set_config(opts)
-          require("luasnip.loaders.from_vscode").lazy_load()
+          require("C410l.plugins.configs.others").luasnip(opts)
         end
       },
 

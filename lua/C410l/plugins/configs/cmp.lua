40e1b61b521e7d1350601389c116ebf01cd6bcf2
caio86 <caioluiz86@gmail.com>
2024-03-13 21:20:53 -0300

    refactor cmp config

--- lua/C410l/plugins/configs/cmp.lua
+++ lua/C410l/plugins/configs/cmp.lua
@@ -3,7 +3,7 @@ local cmp = require "cmp"
 local cmp_ui = {
   icons = true,
   lspkind_text = true,
-  style = "default", -- default/flat_light/flat_dark/atom/atom_colored
+  style = "atom", -- default/flat_light/flat_dark/atom/atom_colored
   border_color = "grey_fg", -- only applicable for "default" style, use color names from base30 variables
   selected_item_bg = "simple", -- colored / simple
 }
@@ -18,21 +18,13 @@ local formatting_style = {
   -- default fields order i.e completion word + item.kind + item.kind icons
   fields = field_arrangement[cmp_style] or { "abbr", "kind", "menu" },
 
-  format = function(_, item)
-    --jlocal icons = require "nvchad.icons.lspkind"
-    local icon = "" --(cmp_ui.icons and icons[item.kind]) or ""
-
-    if cmp_style == "atom" or cmp_style == "atom_colored" then
-      icon = " " .. icon .. " "
-      item.menu = cmp_ui.lspkind_text and "   (" .. item.kind .. ")" or ""
-      item.kind = icon
-    else
-      icon = cmp_ui.lspkind_text and (" " .. icon .. " ") or icon
-      item.kind = string.format("%s %s", icon, cmp_ui.lspkind_text and item.kind or "")
-    end
-
-    return item
-  end,
+  format = require("lspkind").cmp_format({ with_text = true, maxheight = 20, maxwidth = 50, menu = {
+    buffer = "[Buffer]",
+    nvim_lsp = "[LSP]",
+    path = "[Path]",
+    luasnip = "[LuaSnip]",
+    nvim_lua = "[Lua]",
+  }}),
 }
 
 local function border(hl_name)
@@ -49,28 +41,28 @@ local function border(hl_name)
 end
 
 local options = {
-  completion = {
-    completeopt = "menu,menuone",
-  },
-
-  window = {
-    completion = {
-      side_padding = (cmp_style ~= "atom" and cmp_style ~= "atom_colored") and 1 or 0,
-      winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
-      scrollbar = false,
-    },
-    documentation = {
-      border = border "CmpDocBorder",
-      winhighlight = "Normal:CmpDoc",
-    },
-  },
+  -- window = {
+  --   completion = {
+  --     side_padding = (cmp_style ~= "atom" and cmp_style ~= "atom_colored") and 1 or 0,
+  --     winhighlight = "Normal:CmpPmenu,CursorLine:CmpSel,Search:None",
+  --     scrollbar = true,
+  --   },
+  --   documentation = {
+  --     border = border "CmpDocBorder",
+  --     winhighlight = "Normal:CmpDoc",
+  --   },
+  -- },
   snippet = {
     expand = function(args)
       require("luasnip").lsp_expand(args.body)
     end,
   },
+  -- experimental = {
+  --   native_menu = false,
+  --   ghost_text = false,
+  -- },
 
-  --formatting = formatting_style,
+  formatting = formatting_style,
 
   mapping = {
     ["<C-p>"] = cmp.mapping.select_prev_item(),
@@ -98,53 +90,4 @@ if cmp_style ~= "atom" and cmp_style ~= "atom_colored" then
   options.window.completion.border = border "CmpBorder"
 end
 
-return {
-  sources = {
-      { name = "nvim_lsp" },
-      { name = "buffer" },
-      { name = "path" },
-      { name = "luasnip" },
-      { name = "nvim_lua" },
-  },
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-   end,
-  },
-  mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Insert,
-      select = true,
-    },
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif require("luasnip").expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
-      else
-        fallback()
-      end
-    end, {
-        "i",
-        "s",
-      }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif require("luasnip").jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
-      else
-        fallback()
-      end
-    end, {
-        "i",
-        "s",
-      }),
-  },
-}
+return options

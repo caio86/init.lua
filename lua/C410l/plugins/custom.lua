37760492191df90054eeb830c4f09f056b68fb4b
caio86 <caioluiz86@gmail.com>
2024-03-21 18:14:04 -0300

    add conform plugin for autoformatting

--- lua/C410l/plugins/custom.lua
+++ lua/C410l/plugins/custom.lua
@@ -1,8 +1,10 @@
 local plugins = {
+
   {
     "Eandrju/cellular-automaton.nvim",
     event = "VeryLazy",
   },
+
   {
     "dstein64/vim-startuptime",
     cmd = "StartupTime",
@@ -10,6 +12,7 @@ local plugins = {
       vim.g.startuptime_tries = 10
     end,
   },
+
   {
     "christoomey/vim-tmux-navigator",
     keys = {
@@ -27,6 +30,32 @@ local plugins = {
     opts = { signs = false },
   },
 
+  { -- Autoformat
+    "stevearc/conform.nvim",
+    opts = {
+      notify_on_error = false,
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
+      formatters_by_ft = {
+        lua = { "stylua" },
+        nix = { "nixpkgs_fmt" },
+        -- Conform can also run multiple formatters sequentially
+        python = { "isort", "black" },
+        --
+        -- You can use a sub-list to tell conform to run *until* a formatter
+        -- is found.
+        javascript = { { "prettierd", "prettier" } },
+      },
+    },
+  },
 }
 
 return plugins

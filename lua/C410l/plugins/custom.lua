92674c424ec390211296c4012ac0b608b2b18348
caio86 <caioluiz86@gmail.com>
2024-04-12 19:08:53 -0300

    add keymap to toggle format on save

--- lua/C410l/plugins/custom.lua
+++ lua/C410l/plugins/custom.lua
@@ -25,16 +25,19 @@ local plugins = {
 
   {
     "folke/todo-comments.nvim",
-    event = "VimEnter",
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = { signs = false },
+    opts = {},
   },
 
   { -- Autoformat
     "stevearc/conform.nvim",
+    event = "VimEnter",
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
+        if vim.g.disable_autoformat then
+          return
+        end
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
@@ -54,6 +57,21 @@ local plugins = {
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         javascript = { { "prettierd", "prettier" } },
+      },
+    },
+    keys = {
+      {
+        "<leader>uf",
+        function()
+          local Util = require("lazy.core.util")
+          vim.g.disable_autoformat = not vim.g.disable_autoformat
+          if vim.g.disable_autoformat then
+            Util.warn("Disabled auto formatting", { title = "Option" })
+          else
+            Util.info("Enabled auto formatting", { title = "Option" })
+          end
+        end,
+        desc = "Toggle auto formatting",
       },
     },
   },

f14877c74adc442a2d22f062b10d9caf2446de0d
caio86 <caioluiz86@gmail.com>
2024-04-30 14:54:16 -0300

    add database connection inside neovim

--- lua/C410l/plugins/custom.lua
+++ lua/C410l/plugins/custom.lua
@@ -33,6 +33,27 @@ local plugins = {
     "towolf/vim-helm",
     ft = "helm",
   },
+
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true },
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    keys = {
+      { "<leader>D", "<cmd>DBUI<cr>", desc = "Database UI" },
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+    end,
+  },
 }
 
 return plugins

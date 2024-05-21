b0f54ef3e1ca6797b4a23e14b0343f2090c0a9ed
caio86 <caioluiz86@gmail.com>
2024-05-21 20:10:13 -0300

    add bufferline plugin

--- lua/C410l/plugins/ui.lua
+++ lua/C410l/plugins/ui.lua
@@ -18,6 +18,52 @@ return {
     end,
   },
   {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+      { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+      { "<leader>bo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers" },
+      { "<leader>br", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+      { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+      { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer [BF]" },
+      { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer [BF]" },
+      { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer [BF]" },
+      { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer [BF]" },
+    },
+    opts = {
+      options = {
+        diagnostics = "nvim_lsp",
+        always_show_bufferline = false,
+        diagnostics_indicator = function(_, _, diag)
+          local icons = require("C410l.icons").diagnostics
+          local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+            .. (diag.warning and icons.Warn .. diag.warning or "")
+          return vim.trim(ret)
+        end,
+        offsets = {
+          {
+            filetype = "neo-tree",
+            text = "Neo-tree",
+            highlight = "Directory",
+            text_align = "left",
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require("bufferline").setup(opts)
+      -- Fix bufferline when restoring a session
+      vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+        callback = function()
+          vim.schedule(function()
+            pcall(nvim_bufferline)
+          end)
+        end,
+      })
+    end,
+  },
+  {
     "RRethy/vim-illuminate",
     event = "VeryLazy",
     opts = {

fa8c573131c572ffa963ffb2d23f675752e953d0
caio86 <caioluiz86@gmail.com>
2024-08-23 12:35:48 -0300

    feat: add markview plugin

--- lua/C410l/plugins/custom.lua
+++ lua/C410l/plugins/custom.lua
@@ -27,6 +27,26 @@ local plugins = {
   },
 
   {
+    "OXY2DEV/markview.nvim",
+    ft = "markdown",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      modes = { "n", "i", "no", "c" },
+      hybrid_modes = { "i" },
+
+      -- This is nice to have
+      callbacks = {
+        on_enable = function(_, win)
+          vim.wo[win].conceallevel = 2
+          vim.wo[win].concealcursor = "nc"
+        end,
+      },
+    },
+  },
+
+  {
     "kristijanhusak/vim-dadbod-ui",
     dependencies = {
       { "tpope/vim-dadbod", lazy = true },

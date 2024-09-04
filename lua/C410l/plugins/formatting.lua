ed99fc3e9adf26d10a00ed5bb61063b9f253afde
caio86 <caioluiz86@gmail.com>
2024-09-04 19:57:22 -0300

    add trim_whitespace as a fallback for conform.nvim

--- lua/C410l/plugins/formatting.lua
+++ lua/C410l/plugins/formatting.lua
@@ -28,6 +28,8 @@ return {
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         javascript = { { "prettierd", "prettier" } },
+
+        ["_"] = { "trim_whitespace", "prettierd" },
       },
     },
     keys = {

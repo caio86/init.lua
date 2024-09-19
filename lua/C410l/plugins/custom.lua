ef4960b57649a7a571d0f970dc001fcab5c422f3
caio86 <caioluiz86@gmail.com>
2024-09-18 21:33:02 -0300

    feat: add codesnap.nvim plugin

--- lua/C410l/plugins/custom.lua
+++ lua/C410l/plugins/custom.lua
@@ -83,6 +83,30 @@ local plugins = {
       require("codeium").setup(opts)
     end,
   },
+
+  {
+    "mistricky/codesnap.nvim",
+    build = "make",
+    keys = {
+      { "<leader>cs", ":CodeSnap<CR>", desc = "Snapshot", mode = { "v" } },
+    },
+    opts = {
+      mac_window_bar = true,
+      title = "CodeSnap.nvim",
+      code_font_family = "CaskaydiaCove Nerd Font",
+      watermark_font_family = "Pacifico",
+      watermark = "",
+      bg_theme = "default",
+      breadcrumbs_separator = "/",
+      has_breadcrumbs = false,
+      has_line_number = false,
+      show_workspace = false,
+      min_width = 0,
+      bg_x_padding = 122,
+      bg_y_padding = 82,
+      save_path = os.getenv("XDG_PICTURES_DIR") or (os.getenv("HOME") .. "/Pictures"),
+    },
+  },
 }
 
 return plugins

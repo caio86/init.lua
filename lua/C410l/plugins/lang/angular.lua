7b4c100107126898431c2e9631df989f54d728c1
caio86 <caioluiz86@gmail.com>
2024-09-21 19:38:15 -0300

    create angular config

---
+++ lua/C410l/plugins/lang/angular.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "angular", "scss" })
+      end
+      vim.api.nvim_create_autocmd({ "BufReadPost", "BufNewFile" }, {
+        pattern = { "*.component.html", "*.container.html" },
+        callback = function()
+          vim.treesitter.start(nil, "angular")
+        end,
+      })
+    end,
+  },
+}

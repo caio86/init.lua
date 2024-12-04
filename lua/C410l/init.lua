6d7e21eec2a20f905076cda63a5f5be51808f937
caio86 <caioluiz86@gmail.com>
2024-12-04 01:00:08 -0300

    feat: enhance Neovim configuration with improved LSP and autocommands
    - Refactor autocmd definitions for better readability.
      - Added `BufWritePre` autocmd to remove trailing whitespace on save.
      - Introduced `LspAttach` autocmd for dynamic LSP actions on buffer attach.
    
    - Revamp LSP configuration:
      - Modularized server setup with extended capabilities.
      - Enabled inlay hints, codelens, and document highlighting.
      - Added diagnostic configuration with custom icons and severity sorting.
    
    - Integrate `mason.nvim` for automatic installation of essential tools:
      - Ensure installation of `stylua` and `shfmt`.
      - Dynamically trigger FileType events for newly installed LSP servers.
    
    - Update `nvim-cmp` configuration:
      - Re-enabled `buffer` source for completion.

--- lua/C410l/init.lua
+++ lua/C410l/init.lua
@@ -1,14 +1,33 @@
 require("C410l.set")
 require("C410l.remaps")
 require("C410l.lazy")
+local on_attach = require("C410l.plugins.configs.base-lsp").on_attach
+
+local autocmd = vim.api.nvim_create_autocmd
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
 --  See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd("TextYankPost", {
+autocmd("TextYankPost", {
   desc = "Highlight when yanking (copying) text",
   group = vim.api.nvim_create_augroup("kickstart-highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+autocmd({ "BufWritePre" }, {
+  pattern = "*",
+  command = [[%s/\s\+$//e]],
+})
+
+autocmd("LspAttach", {
+  desc = "LSP actions",
+  callback = function(e)
+    local buffer = e.buf
+    local client = vim.lsp.get_client_by_id(e.data.client_id)
+    if client then
+      return on_attach(client, buffer)
+    end
   end,
 })

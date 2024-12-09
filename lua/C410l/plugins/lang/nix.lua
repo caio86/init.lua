5c9d03af1b8b5c4cde0cd184b6a8144816e21f74
caio86 <caioluiz86@gmail.com>
2024-12-09 13:26:46 -0300

    nix lsp and formatting config

---
+++ lua/C410l/plugins/lang/nix.lua
@@ -0,0 +1,25 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = { ensure_installed = { "nix" } },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        nil_ls = {},
+      },
+    },
+  },
+
+  {
+    "stevearc/conform.nvim",
+    optional = true,
+    opts = {
+      formatters_by_ft = {
+        nix = { "nixfmt" },
+      },
+    },
+  },
+}
